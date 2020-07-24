c18df13516234eba14cadfb8cded0f99d0416acc
khoitran20042000@gmail.com <khoitran20042000@gmail.com>
2020-07-24 16:22:08 +1200

    added react2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.notes = [
+      {
+        id: 1,
+        text: 'I have a pair of Jordan 1'
+      },
+      {
+        id: 2,
+        text: 'I have a pair of Jordan 3'
+      },
+      {
+        id: 3,
+        text: 'I have a pair of Jordan 4'
+      },
+      {
+        id: 4,
+        text: 'I have a pair of Jordan 11'
+      }
+    ]
+  }
+
+  render(){
+    return(
+      <div className="wrap">
+        <header>Item you must have</header>
+        <div className="container">
+          <div className="notes">
+            {
+              this.notes.map((note)=>{
+                return(
+                  <div className="note one">
+                    <input type="checkbox" id="first" name="first"></input>
+                    <label for="first">{note.text}</label>
+                </div>
+                )
+              })
+            }
+            <form action="#">
+                <div className="note btn">
+                  <button type="submit" className="btn btn-primary">Done</button>
+                </div>
+            </form>
+          </div>
+        </div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
